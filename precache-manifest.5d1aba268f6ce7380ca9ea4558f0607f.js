aa4347a6c4118cf7ea0898786f8b35309a9ac1a0
Rui Agostinho <rui.goncalves.agostinho@gmail.com>
2019-02-07 17:37:35 +0000

    Updates

---
+++ precache-manifest.5d1aba268f6ce7380ca9ea4558f0607f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LabMov/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "892324f1a2a439463e64",
+    "url": "/LabMov/static/js/runtime~main.892324f1.js"
+  },
+  {
+    "revision": "aa96b3ef57d359e25d2a",
+    "url": "/LabMov/static/js/main.aa96b3ef.chunk.js"
+  },
+  {
+    "revision": "c1a02d7433bde48f6c7c",
+    "url": "/LabMov/static/js/1.c1a02d74.chunk.js"
+  },
+  {
+    "revision": "aa96b3ef57d359e25d2a",
+    "url": "/LabMov/static/css/main.d8ccd80b.chunk.css"
+  },
+  {
+    "revision": "c1a02d7433bde48f6c7c",
+    "url": "/LabMov/static/css/1.402d350f.chunk.css"
+  },
+  {
+    "revision": "72ea820923217465f2992a0fe7984a96",
+    "url": "/LabMov/index.html"
+  }
+];
